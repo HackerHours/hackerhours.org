072ce5ebc402213c3c7b6ebd0cc5ae7354f81310
Aidan Feldman <aidan.feldman@gmail.com>
2012-05-12 04:05:36 -0400

    ensure they've seen the animation for a minimum amount of time

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 (function(){
-  var shuffler;
+  var start = Date.now(),
+    shuffler;
 
   $.get('http://jsonp.jit.su/?url=https%3A%2F%2Fapi.meetup.com%2F2%2Fevents%3Fkey%3D3c2a67534a193f12c46115f7b112e1e%26sign%3Dtrue%26group_urlname%3Dnyhacker%26page%3D3', function(data){
     var results = data.results;
@@ -11,12 +12,19 @@
         $(function(){
           $('.meetupLink').attr('href', result.event_url);
 
-          var dateStr = moment(parseInt(result.time, 10)).format('ddd, MMMM Do YYYY, h:mm a'),
-            $meetupDate = $('#meetupDate');
+          // ensure they've seen the animation for a minimum amount of time
+          var elapsed = Date.now() - start,
+            wait = 1500 - elapsed;
 
-          clearInterval(shuffler);
-          $meetupDate.find('#dateMs').text(result.time);
-          $meetupDate.find('#dateStr').text(dateStr);
+          if (wait < 0) wait = 0;
+          setTimeout(function(){
+            var dateStr = moment(parseInt(result.time, 10)).format('ddd, MMMM Do YYYY, h:mm a'),
+              $meetupDate = $('#meetupDate');
+
+            clearInterval(shuffler);
+            $meetupDate.find('#dateMs').text(result.time);
+            $meetupDate.find('#dateStr').text(dateStr);
+          }, wait);
         });
 
         return;
